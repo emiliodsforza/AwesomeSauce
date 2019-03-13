8fa42dc56263aa620655f78878f537bfff8743e7
Emilio D Sforza <esforza@emiliodsforza.com>
2019-03-13 06:58:56 -0400

    testing commit

--- AwesomeSauce/Program.cs
+++ AwesomeSauce/Program.cs
@@ -20,5 +20,6 @@ namespace AwesomeSauce
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+        //testing push
     }
 }
